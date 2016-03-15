62b5e29a4edd51ffb86ba6087c5e180a26f66894
Ronny Ewanek <ronny.ewanek@gmail.com>
2016-03-15 16:59:15 -0500

    fixed routing issue

--- app.js
+++ app.js
@@ -1,68 +1,92 @@
-// Factory
-angular.module('flapperNews', ['ui.router'])
-	.factory('posts', [function() {
-		var o = {
-			posts: []
-		};
-		return o; 
-	}])
-	.controller('MainCtrl', [
-		'$scope',
-		'posts',
-		function($scope, posts) {
-			$scope.posts = posts.posts;
-		}])
 
+	
+
+//UI routing
 angular.module('flapperNews', ['ui.router'])
 	.config([
 		'$stateProvider',
 		'$urlRouterProvider',
 		function($stateProvider, $urlRouterProvider) {
+
 			$stateProvider
 				.state('home', {
 					url: '/home',
 					templateUrl: '/home.html',
 					controller: 'MainCtrl'
-				});
+				})
+
+				.state('posts', {
+					url: '/posts/{id}',
+					templateUrl: '/posts.html',
+					controller: 'PostsCtrl'
+			});
 				$urlRouterProvider.otherwise('home');
 		}])
 
+	
+// Factory
 
 
-angular.module("flapperNews", ['ui.router']).controller("MainCtrl", ["$scope", 'posts', function($scope, posts){
-	$scope.test = "Hello world!";
-	$scope.posts = [
-		{title: 'post 1', upvotes: 5},
-		{title: 'post 2', upvotes: 2},
-		{title: 'post 3', upvotes: 15},
-		{title: 'post 4', upvotes: 9},
-		{title: 'post 5', upvotes: 4}
-	];
+//controllers
 
-	$scope.addPost = function() {
-		if(!scope.title || $scope.title === '') { return; }
-		$scope.posts.push({title: $scope.title, upvotes: 0});
-		$scope.title = ""; //clears input box after post is created
-	};
+	.controller('MainCtrl', [
+		'$scope', 
+		'posts', 
+		function($scope, posts){
+			$scope.posts = posts.posts;
+			$scope.test = "Hello world!";
+			$scope.posts = [
+				{title: 'post 1', upvotes: 5},
+				{title: 'post 2', upvotes: 2},
+				{title: 'post 3', upvotes: 15},
+				{title: 'post 4', upvotes: 9},
+				{title: 'post 5', upvotes: 4}
+			];
 
-	$scope.incrementUpvotes = function(post) {
-		post.upvotes += 1; 
-	};
-
-	$scope.addPost = function() {
-		if(!$scope.title || $scope.title === '') {return;}
-		$scope.posts.push({
-			title: $scope.title,
-			link: $scope.link,
-			upvotes: 0
-		});
-		$scope.title = '';
-		$scope.link	= '';
-	};
+			$scope.addPost = function() {
+				// if(!scope.title || $scope.title === '') { return; }
+				$scope.posts.push({
+					title: $scope.title, 
+					link: $scope.link,
+					upvotes: 0,
+					comments: [
+						{author: 'Joe', body: 'Cool post!', upvotes: 0},
+						{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+					]
+				});
 
 
+				$scope.title = ""; //clears input box after post is created
+			};
 
-}]);
+			$scope.incrementUpvotes = function(post) {
+				post.upvotes += 1; 
+			};
+
+			
+		}])
+
+	.controller('PostsCtrl', [
+		'$scope',
+		'$stateParams',
+		'posts',
+		function($scope, $stateParams, posts) {
+			$scope.post = posts.posts[$stateParams.id];
+	}])
+
+
+	.factory('posts', [function() {
+		var o = {
+			posts: []
+		};
+		return o; 
+	}]);
+	// .controller('MainCtrl', [
+	// 	'$scope',
+	// 	'posts',
+	// 	function($scope, posts) {
+	// 		$scope.posts = posts.posts;
+	// 	}]);
 
 
 
