f88dafd6232a238d48b8789c4b55030462f07dd3
Ronny Ewanek <ronny.ewanek@gmail.com>
2016-03-21 17:09:30 -0500

    add devise

--- ng-reddit/app/controllers/application_controller.rb
+++ ng-reddit/app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
 class ApplicationController < ActionController::Base
-	def angular
-		render 'layouts/application'
-	end
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   respond_to :json
+
+
+	def angular
+		render 'layouts/application'
+	end
+
+  private
+
+  def configure_permitted_parameters
+  	devise_parameters_santizer.for(:sign_up) << :username
+  end
 end
