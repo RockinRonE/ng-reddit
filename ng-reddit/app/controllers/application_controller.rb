9a52158b47dc9317202fed85eee2e277ed5c2060
Ronny Ewanek <ronny.ewanek@gmail.com>
2016-03-22 11:13:45 -0500

    add completed app

--- ng-reddit/app/controllers/application_controller.rb
+++ ng-reddit/app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -7,6 +6,7 @@ class ApplicationController < ActionController::Base
 
   respond_to :json
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 	def angular
 		render 'layouts/application'
@@ -15,6 +15,6 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-  	devise_parameters_santizer.for(:sign_up) << :username
+  	devise_parameter_sanitizer.for(:sign_up) << :username
   end
 end
